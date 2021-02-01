1be4426c903173d4004c4e350a4a710e28026948
Yaakov <dbrionesmtz@yahoo.com>
2021-02-01 11:30:17 -0600

    Se gregaron la vista del login

---
+++ views/header2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador</title>
+    <script src="https://kit.fontawesome.com/471047becf.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo URL ?>public/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="<?php echo URL ?>public/css/Chart.css">
+    <link rel="stylesheet" href="<?php echo URL ?>public/css/style.css">
+</head>
+
+<body>
+    <header class="header">
+        <div>
+            <a href="#">
+                <h1 class="header__logo">El Puestito</h1>
+            </a>
+        </div>
+    </header>
+    <div class="main_container main_container-login">
