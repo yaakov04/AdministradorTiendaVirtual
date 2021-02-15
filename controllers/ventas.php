6bb71e7043078280f872569f81b989adbbd4a4eb
Yaakov <dbrionesmtz@yahoo.com>
2021-02-15 17:45:38 -0600

    Se muestran las consultas de las ventas pendientes

--- controllers/ventas.php
+++ controllers/ventas.php
@@ -15,7 +15,7 @@ class Ventas extends Controller{
                 if (isset($ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']])) {
                     //existe->no hagas nada
                 }else{
-                    //existe->registra pedido
+                    //existe->agrega pedido
                     $ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']]=array(
                         'nombre_producto' =>$resultado['nombre_producto'],
                         'cantidad_producto'=>$resultado['cantidad_articulo'],
@@ -24,7 +24,7 @@ class Ventas extends Controller{
                 }
                 
             }else{
-                //¬exista->registraVenta
+                //¬exista->agregaVenta
                 $total=new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
                 $total=$total->formatCurrency($resultado['total'], 'MXN');
                 $precio =new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
@@ -49,7 +49,7 @@ class Ventas extends Controller{
         }//while
         $this->view->ventas=$ventas;
         $this->view->render('ventas/index');
-    }
+    }//
     function completadas(){
         $consultaDB=$this->model->getVentasCompletadas();
         $ventas=array();
@@ -60,7 +60,7 @@ class Ventas extends Controller{
                 if (isset($ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']])) {
                     //existe->no hagas nada
                 }else{
-                    //existe->registra pedido
+                    //existe->agrega pedido
                     $ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']]=array(
                         'nombre_producto' =>$resultado['nombre_producto'],
                         'cantidad_producto'=>$resultado['cantidad_articulo'],
@@ -69,7 +69,7 @@ class Ventas extends Controller{
                 }
                 
             }else{
-                //¬exista->registraVenta
+                //¬exista->agregaVenta
                 $total=new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
                 $total=$total->formatCurrency($resultado['total'], 'MXN');
                 $precio =new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
@@ -94,8 +94,50 @@ class Ventas extends Controller{
         }//while
         $this->view->ventas=$ventas;
         $this->view->render('ventas/completadas');
-    }
+    }//
     function pendientes(){
+        $consultaDB=$this->model->getVentasPendientes();
+        $ventas=array();
+        while ($resultado =$consultaDB->fetch_assoc()) {
+            //comprobar si existe venta
+            if (isset($ventas[$resultado['id_venta']])) {
+                //existe->comprueba si existe pedido
+                if (isset($ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']])) {
+                    //existe->no hagas nada
+                }else{
+                    //existe->agrega pedido
+                    $ventas[$resultado['id_venta']]['detalles_pedido'][$resultado['id_pedido']]=array(
+                        'nombre_producto' =>$resultado['nombre_producto'],
+                        'cantidad_producto'=>$resultado['cantidad_articulo'],
+                        'precio_producto'=>$resultado['precio_articulo']
+                    );
+                }
+                
+            }else{
+                //¬exista->agregaVenta
+                $total=new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
+                $total=$total->formatCurrency($resultado['total'], 'MXN');
+                $precio =new NumberFormatter( 'es_MX', NumberFormatter::CURRENCY );
+                $precio=$precio->formatCurrency((int)$resultado['precio_articulo'], 'MXN');
+                $ventas[$resultado['id_venta']]=array(
+                    'id_venta'          =>$resultado['id_venta'],
+                    'estatus'           =>$resultado['estatus'],
+                    'comprador'         =>$resultado['comprador'],
+                    'fecha_venta'       =>$resultado['fecha'],
+                    'detalles_pedido'   =>array(
+                        $resultado['id_pedido']     =>array(
+                            'nombre_producto' =>$resultado['nombre_producto'],
+                            'cantidad_producto'=>$resultado['cantidad_articulo'],
+                            'precio_producto'=>$precio
+                        )
+                    ),
+                    'datos_envio'       =>$resultado['datos_envio'],
+                    'total'             =>$total
+                );
+            }
+            
+        }//while
+        $this->view->ventas=$ventas;
         $this->view->render('ventas/pendientes');
     }
 }
--- models/ventasModel.php
+++ models/ventasModel.php
@@ -34,4 +34,19 @@ class ventasModel extends Model{
             return 'Error '. $e;
         }
     }//
+    public function getVentasPendientes(){
+        try{
+            $conexion=$this->db->conexion();
+            $sql=" SELECT ventas.id as id_venta, estados.estado as estatus, usuarios.nombre as comprador, DATE(ventas.fecha) as fecha, ventas.datos_envio as datos_envio, ventas.total as total, pedidos.id as id_pedido, pedidos.cantidad as cantidad_articulo, pedidos.precio as precio_articulo, productos.nombre_producto as nombre_producto ";
+            $sql.=" FROM ventas INNER JOIN usuarios ON ventas.id_cliente = usuarios.id ";
+            $sql.=" INNER JOIN pedidos ON ventas.id = pedidos.id_venta ";
+            $sql.=" INNER JOIN estados ON ventas.estatus = estados.id ";
+            $sql.=" INNER JOIN productos ON pedidos.id_producto = productos.id WHERE estatus < 2 ";
+            $resultado=$conexion->query($sql);
+            $conexion->close();
+            return $resultado;
+        }catch(Exception $e){
+            return 'Error '. $e;
+        }
+    }//
 }
